b1d7ecae6d7d695bb1f12dc5503b2a28c5a25828
Roosevelt Santana <Rooseveltpss04@gmail.com>
2026-03-11 18:33:37 -0300

    feat: tela de login e de cadastro

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './paginas/Login'
+import Cadastro from './paginas/Cadastro'
 
 function App() {
-  return <Login />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/cadastro" element={<Cadastro />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
