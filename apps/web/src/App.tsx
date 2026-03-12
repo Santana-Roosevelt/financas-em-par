ee03a293fc5740af26cb745a47ee74fd666d55c9
Roosevelt Santana <Rooseveltpss04@gmail.com>
2026-03-12 11:26:54 -0300

    feat: dashboard completo com sidebar e graficos

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './paginas/Login'
 import Cadastro from './paginas/Cadastro'
 import Casal from './paginas/Casal'
+import Dashboard from './paginas/Dashboard'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/casal" element={<Casal />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   )
