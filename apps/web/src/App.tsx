b9dff3fecf4f4c7a5e27436228456569d9130c70
Roosevelt Santana <Rooseveltpss04@gmail.com>
2026-03-12 10:18:25 -0300

    Criação da tela do casal

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Login from './paginas/Login'
 import Cadastro from './paginas/Cadastro'
+import Casal from './paginas/Casal'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
+        <Route path="/casal" element={<Casal />} />
       </Routes>
     </BrowserRouter>
   )
